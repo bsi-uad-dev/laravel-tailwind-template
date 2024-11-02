0dd946a51ff9e5eeb77479138469f618df4bee98
taufiqadityaputra <taufiqadityaputra5@gmail.com>
2024-11-02 11:18:24 +0700

    update logo

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,8 +1,8 @@
 <nav class="fixed w-full z-20 top-0 start-0">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap">{{ config('app.name') }}</span>
+            <img src="{{ asset('img/logo-ec.svg') }}" class="h-5" alt="Flowbite Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap">{{ config('app.name') }}</span>
         </a>
         {{-- <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             @auth
